548dd8cafa981d4fb9b6df6d36d34a6c2f04ae0c
air-one-x <erwanxavierxvi@gmail.com>
2020-08-14 13:54:13 +0200

    banner compagnies homepage

---
+++ src/Data/logos.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        logo: '/pictures/logo/logo1.png'
+    },
+    {
+        logo: '/pictures/logo/logo2.png'
+    },
+    {
+        logo: '/pictures/logo/logo3.png'
+    },
+    {
+        logo: '/pictures/logo/logo4.png'
+    },
+    {
+        logo: '/pictures/logo/logo5.png'
+    },
+    {
+        logo: '/pictures/logo/logo6.png'
+    },
+];
--- src/components/Home/home.component.js
+++ src/components/Home/home.component.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import { v4 as id } from 'uuid';
+
 
 import './home.style.css';
+import Logos from '../../Data/logos';
 
 const Home = () => {
 
     return (
-        <div className="home--page">
+        <div>
 
-            <div className="home--page__content">
-                <h1>Car nos développeurs juniors ont du talents !</h1>
-                <p>Devjunior a était conçu pour donner de la visibilité aux développeurs juniors mais aussi pour montrer aux entreprises que les développeurs juniors ont du talents.  </p>
-                <br></br>
-                <p>CDI, CDD, stage, alternance ou freelance</p>
-                <br></br>
-                <button className="button__inscription">S'inscrire</button>
-                <button className="button__connexion">Se connecter</button>
+
+
+            <div className="home--page">
+                <div className="home--page__content">
+                    <h1>Car nos développeurs juniors ont du talents !</h1>
+                    <p>Devjunior a était conçu pour donner de la visibilité aux développeurs juniors mais aussi pour montrer aux entreprises que les développeurs juniors ont du talents.  </p>
+                    <br></br>
+                    <p>CDI, CDD, stage, alternance ou freelance</p>
+                    <br></br>
+                    <button className="button__inscription">S'inscrire</button>
+                    <button className="button__connexion">Se connecter</button>
+                </div>
+            <div className="home--page__picture"></div>
             </div>
 
-            <div className="home--page__picture"></div>
 
+
+            <ul className="banner--compagnies row">
+                {
+                    Logos.map((logo)=> (
+                    <li 
+                        className="col-sm-4 col-lg-2 col-4"
+                        key={id()}
+                        style={{
+                            backgroundImage: `url(${logo.logo})`,
+                            width: '100px',
+                            height: '100px',
+                            backgroundSize: 'contain',
+                            backgroundPosition : 'center',
+                            backgroundRepeat: 'no-repeat'
+                        }}>
+                    </li>))
+                }
+            </ul>
 
         </div>
     );
